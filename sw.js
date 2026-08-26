b052f93660ee65bcab7afdaba752228d17896807
RiseUpYouthGroup <karlafoureaux@gmail.com>
2026-08-26 17:31:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='rise-up-v6';
-const FILES=['./','index.html','manifest.json','rise-up-logo.png','icon-192.png','icon-512.png'];
+const CACHE='rise-up-v7';
+const FILES=['./','index.html','manifest.json','rise-up-logo.png','icon-192.png','icon-512.png','zelle-qr.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
